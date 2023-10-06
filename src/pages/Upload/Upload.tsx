c2fdd6bcffcb01a2d4bdf5782ff4627c6ead0b10
sarahishamsaied <sarahishamsaied@gmail.com>
2023-10-06 11:14:51 +0300

    cloudinary

--- src/pages/Upload/Upload.tsx
+++ src/pages/Upload/Upload.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Cloudinary } from "cloudinary-core";
+
+const cloudinary = new Cloudinary({ cloud_name: "dcqlhlybd" });
 
 const Upload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -8,20 +11,20 @@ const Upload = () => {
     setSelectedFile(e.target.files[0]);
   };
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    console.log(e);
+  const handleUpload = async () => {
     if (!selectedFile) {
-      alert("Please select a TIFF image.");
+      alert("Please select an image.");
       return;
     }
 
+    // Upload the selected file to Cloudinary
     const formData = new FormData();
-    formData.append("file_url", selectedFile);
+    formData.append("file", selectedFile);
+    formData.append("upload_preset", "ukv0dwix"); // Replace with your Cloudinary upload preset
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/predict",
+        "https://api.cloudinary.com/v1_1/dcqlhlybd/image/upload",
         formData,
         {
           headers: {
@@ -29,21 +32,40 @@ const Upload = () => {
           },
         }
       );
-      console.log(response.data);
+
+      // Handle the Cloudinary response, which contains the image URL
+      const imageUrl = response.data.secure_url;
+
+      // Now, you can send this URL to your Flask API
+      sendToFlaskApi(imageUrl);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const sendToFlaskApi = (imageUrl: any) => {
+    // Send the image URL to your Flask API using axios or any other method
+    axios
+      .post("http://localhost:5000/predict", { file_url: imageUrl })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div>
-      <h1>TIFF Image Upload</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="file" accept=".tiff" onChange={handleFileChange} />
-        <button type="submit" className="bg-blue-500 p-4 rounded-sm">
-          Submit
-        </button>
-      </form>
+      <h1>Image Upload</h1>
+      <input
+        type="file"
+        accept=".jpg, .jpeg, .png"
+        onChange={handleFileChange}
+      />
+      <button onClick={handleUpload} className="bg-blue-500 p-4 rounded-sm">
+        Upload
+      </button>
     </div>
   );
 };
